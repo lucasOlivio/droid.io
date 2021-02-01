23c3ec8221a40d942c50afcbd912dbbf223efb64
Lucas Olivio <lucas27_olivio@hotmail.com>
2021-02-01 10:03:51 -0300

    Initial commit

---
+++ server/droidio/config/local.py
@@ -0,0 +1,23 @@
+import os
+from .common import *
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+DEBUG = True
+
+# Testing
+INSTALLED_APPS += ('django_nose',)
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = [
+    BASE_DIR,
+    '-s',
+    '--nologcapture',
+    '--with-coverage',
+    '--with-progressive',
+    '--cover-package=droidio'
+]
+
+# Mail
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ server/droidio/wsgi.py
@@ -0,0 +1,12 @@
+"""
+WSGI config for droidio project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/gunicorn/
+"""
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "droidio.config.production")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
