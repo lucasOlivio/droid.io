d5b4ab3f87dc4984ee3a8c1d8930d0734805cfdd
Lucas Olivio <lucas27_olivio@hotmail.com>
2021-02-02 15:59:55 -0300

    Added postman collection for tests

--- server/droidio/users/models.py
+++ server/droidio/users/models.py
@@ -13,9 +13,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
