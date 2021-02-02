118943da484def00230fc8588917e1f91e9ab341
Lucas Olivio <lucas27_olivio@hotmail.com>
2021-02-02 11:24:25 -0300

    Authentication with jwt

--- server/droidio/urls.py
+++ server/droidio/urls.py
@@ -7,6 +7,11 @@ from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from .users.views import UserViewSet, UserCreateViewSet
 from .demands.views import DemandViewSet
 
@@ -19,6 +24,7 @@ router.register(r"demands", DemandViewSet, basename="demands")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)),
-    path("api-token-auth/", views.obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     re_path(r"^$", RedirectView.as_view(url=reverse_lazy("api-root"), permanent=False)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
