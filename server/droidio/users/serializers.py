ce2e6b42b3e39bc49e3790dd923797ce2e12a58c
Lucas Olivio <lucas27_olivio@hotmail.com>
2021-02-03 10:44:37 -0300

    clean code

--- server/droidio/users/serializers.py
+++ server/droidio/users/serializers.py
@@ -19,13 +19,6 @@ class CreateUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = (
-            "id",
-            "username",
-            "password",
-            "first_name",
-            "last_name",
-            "email",
-        )
+        fields = ("id", "username", "password", "first_name", "last_name", "email")
         read_only_fields = ("id",)
         extra_kwargs = {"password": {"write_only": True}}
